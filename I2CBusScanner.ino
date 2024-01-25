375a4d9a50398e5330abf9c67104a23194deb87f
Adam Howell <AdamJHowell@users.noreply.github.com>
2024-01-25 16:47:41 -0700

    .clang-format: removed deprecated elements and replaced invalid values. Changed line endings on a few files.

--- I2CBusScanner.ino
+++ I2CBusScanner.ino
@@ -28,114 +28,114 @@ const char unscannedChar = ' ';
 
 void setup()
 {
-	// A short delay to provide time to turn on the serial monitor.
-	delay( 1000 );
-	Serial.begin( 115200 );
-	if( !Serial )
-		delay( 1000 );
-	Serial.printf( "\nRunning setup() in %s.\n", sketchName );
-	Serial.println( __FILE__ );
+   // A short delay to provide time to turn on the serial monitor.
+   delay( 1000 );
+   Serial.begin( 115200 );
+   if( !Serial )
+      delay( 1000 );
+   Serial.printf( "\nRunning setup() in %s.\n", sketchName );
+   Serial.println( __FILE__ );
 
 #ifdef OVERRIDE_WIRE
-	// Override the default I2C GPIOs.
-	Wire.begin( sdaGPIO, sclGPIO );
+   // Override the default I2C GPIOs.
+   Wire.begin( sdaGPIO, sclGPIO );
 #else
-	// Use default I2C GPIOs.
-	 Wire.begin();
+   // Use default I2C GPIOs.
+    Wire.begin();
 #endif
 
-	Serial.println( "Setup complete." );
+   Serial.println( "Setup complete." );
 }
 
 
 void loop()
 {
-	unsigned long time = millis();
-	if( ( lastLoop == 0 ) || ( time - lastLoop ) > loopDelay )
-	{
-		byte error;
-		byte address;
-		byte deviceCount = 0;
-		byte errorCount = 0;
-		byte foundArray[111];
-		byte arrayIndex = 0;
+   unsigned long time = millis();
+   if( ( lastLoop == 0 ) || ( time - lastLoop ) > loopDelay )
+   {
+      byte error;
+      byte address;
+      byte deviceCount = 0;
+      byte errorCount = 0;
+      byte foundArray[111];
+      byte arrayIndex = 0;
 
-		loopCount++;
-		Serial.printf( "\n%s will scan the I2C bus for device addresses in the range of 0x08 to 0x77.\n", sketchName );
+      loopCount++;
+      Serial.printf( "\n%s will scan the I2C bus for device addresses in the range of 0x08 to 0x77.\n", sketchName );
 
 #ifdef OVERRIDE_WIRE
-		Serial.printf( "Using %d for the SDA GPIO.\n", sdaGPIO );
-		Serial.printf( "Using %d for the SCL GPIO.\n", sclGPIO );
+      Serial.printf( "Using %d for the SDA GPIO.\n", sdaGPIO );
+      Serial.printf( "Using %d for the SCL GPIO.\n", sclGPIO );
 #else
-		Serial.println( "Using the default SDA and SCL GPIOs." );
+      Serial.println( "Using the default SDA and SCL GPIOs." );
 #endif
 
-		Serial.printf( "Addresses with a device will be represented by '%c'.\n", foundChar );
-		Serial.printf( "Addresses without a device will be represented by '%c'.\n", emptyChar );
-		Serial.printf( "Addresses which return an error will be represented by '%c'.\n", errorChar );
+      Serial.printf( "Addresses with a device will be represented by '%c'.\n", foundChar );
+      Serial.printf( "Addresses without a device will be represented by '%c'.\n", emptyChar );
+      Serial.printf( "Addresses which return an error will be represented by '%c'.\n", errorChar );
 
-		// 0x00 - 0x07 and 0x78 - 0x7F are reserved I2C address ranges.
-		// Source: https://learn.adafruit.com/i2c-addresses/the-list
-		// Because of these reservations, scanning will begin at 0x08 and end at 0x77 (inclusive).
-		Serial.println( "    0123456789ABCDEF" );
-		Serial.print( "0x0 " );
-		for( int i = 0x00; i < 0x08; i++ )
-			Serial.print( unscannedChar );
+      // 0x00 - 0x07 and 0x78 - 0x7F are reserved I2C address ranges.
+      // Source: https://learn.adafruit.com/i2c-addresses/the-list
+      // Because of these reservations, scanning will begin at 0x08 and end at 0x77 (inclusive).
+      Serial.println( "    0123456789ABCDEF" );
+      Serial.print( "0x0 " );
+      for( int i = 0x00; i < 0x08; i++ )
+         Serial.print( unscannedChar );
 
-		for( address = 0x08; address < 0x78; address++ )
-		{
-			// Print a carriage return and the row.
-			if( address % 0x10 == 0 )
-			{
-				Serial.println( "" );
-				Serial.print( "0x" );
-				// Get the 2nd-least significant hex number.
-				int mostSignificantFigure = address / 0x10;
-				Serial.print( mostSignificantFigure );
-				Serial.print( " " );
-			}
+      for( address = 0x08; address < 0x78; address++ )
+      {
+         // Print a carriage return and the row.
+         if( address % 0x10 == 0 )
+         {
+            Serial.println( "" );
+            Serial.print( "0x" );
+            // Get the 2nd-least significant hex number.
+            int mostSignificantFigure = address / 0x10;
+            Serial.print( mostSignificantFigure );
+            Serial.print( " " );
+         }
 
-			Wire.beginTransmission( address );
-			error = Wire.endTransmission();
-			// If a device is found.
-			if( error == 0 )
-			{
-				Serial.print( foundChar );
-				foundArray[arrayIndex] = address;
-				arrayIndex++;
-				deviceCount++;
-			}
-			// If there is an error at the tested address.
-			else if( error != 2 )
-			{
-				Serial.print( errorChar );
-				errorCount++;
-			}
-			else
-				Serial.print( emptyChar );
-		}
-		Serial.println( "\n" );
+         Wire.beginTransmission( address );
+         error = Wire.endTransmission();
+         // If a device is found.
+         if( error == 0 )
+         {
+            Serial.print( foundChar );
+            foundArray[arrayIndex] = address;
+            arrayIndex++;
+            deviceCount++;
+         }
+         // If there is an error at the tested address.
+         else if( error != 2 )
+         {
+            Serial.print( errorChar );
+            errorCount++;
+         }
+         else
+            Serial.print( emptyChar );
+      }
+      Serial.println( "\n" );
 
-		if( deviceCount == 0 )
-			Serial.println( "No I2C devices found." );
-		else if( deviceCount > 0 )
-		{
-			Serial.print( deviceCount );
-			Serial.print( " device" );
-			if( deviceCount > 1 )
-				Serial.print( "s" );
-			Serial.println( " found." );
-			for( int i = 0; i < arrayIndex; i++ )
-				Serial.printf( "Address: 0x%X\n", foundArray[i] );
-			Serial.println( "" );
-		}
+      if( deviceCount == 0 )
+         Serial.println( "No I2C devices found." );
+      else if( deviceCount > 0 )
+      {
+         Serial.print( deviceCount );
+         Serial.print( " device" );
+         if( deviceCount > 1 )
+            Serial.print( "s" );
+         Serial.println( " found." );
+         for( int i = 0; i < arrayIndex; i++ )
+            Serial.printf( "Address: 0x%X\n", foundArray[i] );
+         Serial.println( "" );
+      }
 
-		if( errorCount > 0 )
-			Serial.printf( "%d scanning errors encountered!\n", errorCount );
+      if( errorCount > 0 )
+         Serial.printf( "%d scanning errors encountered!\n", errorCount );
 
-		Serial.printf( "Scan # %lu complete.\n", loopCount );
+      Serial.printf( "Scan # %lu complete.\n", loopCount );
 
-		Serial.printf( "Pausing for %lu seconds.\n\n", loopDelay / 1000 );
-		lastLoop = millis();
-	}
+      Serial.printf( "Pausing for %lu seconds.\n\n", loopDelay / 1000 );
+      lastLoop = millis();
+   }
 }
--- README.md
+++ README.md
@@ -3,9 +3,11 @@ This is an Arduino-compatible sketch which will scan the I2C bus for devices, re
 Because of address reservations, scanning will only attempt addresses from 0x08 to 0x77.
 The SCL and SDA GPIOs can be set by changing the values of sdaGPIO and sclGPIO.
 
+
 ## Hardware
 
 The only required hardware is an I2C device and a MCU to run this program on.
+
 
 ## Libraries
 
